a96ef7a16bbb61c0c9204f5ac18c83f5db896abd
Bowie Sessions <bowie@Bowies-MacBook-Pro.local>
2019-03-04 23:28:48 -0600

    Readme and some final styling and commenting

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,48 +1,70 @@
-// You should save your application's data inside of app/data/friends.js as an array of objects. Each of these objects should roughly follow the format below.
+// Because we're ridiculous, here's our special variable Javascript file. This seems extra.
 
 var friends = [
   {
-    friendName: "Ahmed",
+    friendName: "Charles Boyle",
     photoURL:
-      "https://www.echotheatercompany.com/wp-content/uploads/2015/05/Ahmed-Best-HeadshotWEB.jpg",
-    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1],
-    routeName: "Ahmed"
+      "https://i0.wp.com/thats-normal.com/wp-content/uploads/2016/01/boyledancing.gif",
+    scores: [5, 2, 2, 3, 3, 3, 4, 5, 2, 1],
+    routeName: "charlesboyle"
   },
   {
-    friendName: "Lynn",
+    friendName: "Raymond Holt",
     photoURL:
-      "https://pbs.twimg.com/profile_images/813827400819609600/bYdCDzxv_400x400.jpg",
-    scores: [1, 5, 5, 1, 2, 5, 3, 2, 1, 5],
-    routeName: "Lynn"
+      "https://66.media.tumblr.com/2dc6b256c39190d8debd32e4331ab708/tumblr_p0vwbxTf8Q1sap6q6o3_500.gif",
+    scores: [2, 2, 3, 2, 2, 5, 3, 2, 1, 1],
+    routeName: "raymondholt"
   },
   {
-    friendName: "Bill",
+    friendName: "Jake Peralta",
     photoURL:
-      "https://pbs.twimg.com/profile_images/813827400819609600/bYdCDzxv_400x400.jpg",
-    scores: [2, 3, 2, 3, 2, 3, 2, 3, 2, 3],
-    routeName: "Bill"
+      "https://media1.tenor.com/images/574f9669c3bac2b6d07edcb317b07e55/tenor.gif?itemid=11852388",
+    scores: [3, 5, 4, 1, 4, 5, 2, 5, 5, 5],
+    routeName: "jakeperalta"
   },
   {
-    friendName: "Timina",
+    friendName: "Amy Santiago",
     photoURL:
-      "https://pbs.twimg.com/profile_images/813827400819609600/bYdCDzxv_400x400.jpg",
-    scores: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5],
-    routeName: "Timina"
+      "https://thumbs.gfycat.com/WellwornDamagedKoodoo-size_restricted.gif",
+    scores: [4, 5, 5, 2, 5, 5, 5, 5, 5, 5],
+    routeName: "amysantiago"
   },
   {
-    friendName: "Gina",
+    friendName: "Gina Linetti",
     photoURL:
-      "https://pbs.twimg.com/profile_images/813827400819609600/bYdCDzxv_400x400.jpg",
+      "https://i.pinimg.com/originals/2f/83/a3/2f83a36208203b69d7192cc27c1d6a22.gif",
     scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    routeName: "Gina"
+    routeName: "ginalinetti"
   },
   {
-    friendName: "Mehlisandre",
+    friendName: "Rosa Diaz",
     photoURL:
-      "https://pbs.twimg.com/profile_images/813827400819609600/bYdCDzxv_400x400.jpg",
+      "https://media1.tenor.com/images/c2de6cf6f2225526f70a6f31a97bad15/tenor.gif",
     scores: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
-    routeName: "Mehlisandre"
+    routeName: "rosadiaz"
+  },
+  {
+    friendName: "Terry Jeffords",
+    photoURL:
+      "https://media2.giphy.com/media/xUOxfjE1FEvpiqvcvC/source.gif",
+    scores: [3, 5, 5, 1, 4, 5, 5, 5, 3, 1],
+    routeName: "terryjeffords"
+  },
+  {
+    friendName: "Adrian Pimento",
+    photoURL:
+      "https://i.imgur.com/tiefGBJ.gif",
+    scores: [1, 3, 3, 2, 5, 1, 5, 2, 3, 4],
+    routeName: "adrianpimento"
+  },
+  {
+    friendName: "Hitchcock and Scully",
+    photoURL:
+      "https://img.buzzfeed.com/buzzfeed-static/static/2018-05/10/7/asset/buzzfeed-prod-web-01/anigif_sub-buzz-15278-1525952169-1.gif?downsize=700:*&output-format=auto&output-quality=auto",
+    scores: [5, 2, 1, 5, 5, 4, 3, 4, 2, 5],
+    routeName: "hitchcockandscully"
   }
 ];
 
+// Cool, here's an export so we can use it. Cool.
 module.exports = friends;
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,24 +1,35 @@
-// Your htmlRoutes.js file should include two routes:
+// Dependencies
 var path = require("path");
 var express = require("express");
 var app = express();
-// A GET Route to /survey which should display the survey page.
-// A default, catch-all route that leads to home.html which displays the home page.
 
+// Default catch-all path sending you on home
 app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/home.html"));
-  });
-  
-  app.get("/main.css", function(req, res) {
-      res.sendFile(path.join(__dirname, "../../main.css"));
-    });
-  
-  app.get("/survey", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/survey.html"));
-  });
+  res.sendFile(path.join(__dirname, "../public/home.html"));
+});
 
-  app.get("/favicon.ico", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/favicon.ico"))
-  })
+// How to feed in our main.css file to all our pages.
+app.get("/main.css", function(req, res) {
+  res.sendFile(path.join(__dirname, "../../main.css"));
+});
 
-  module.exports = app;
+// Because links to images are hard.
+app.get("/b99.jpg", function(req, res) {
+  res.sendFile(path.join(__dirname, "../public/b99.jpg"));
+});
+app.get("/b99logo.png", function(req, res) {
+  res.sendFile(path.join(__dirname, "../public/b99logo.png"));
+});
+
+// Loads up the Survey page.
+app.get("/survey", function(req, res) {
+  res.sendFile(path.join(__dirname, "../public/survey.html"));
+});
+
+// Gives us our pretty favicon, THANKS HEROKU.
+app.get("/favicon.ico", function(req, res) {
+  res.sendFile(path.join(__dirname, "../public/favicon.ico"));
+});
+
+// Load it up! Send it out! RAWHIDE!
+module.exports = app;
