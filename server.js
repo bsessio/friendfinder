e40a371fa42718d4dc9245e532d7a3cf3ccc89e0
Bowie Sessions <bowie@Bowies-MacBook-Pro.local>
2019-03-04 20:04:30 -0600

    PORT? Please? Any PORT in a storm?

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
 // Dependencies
 // =============================================================
 var express = require("express");
-var path = require("path");
 var htmlRoutes = require("./app/routing/htmlRoutes");
 var apiRoutes = require("./app/routing/apiRoutes");
-var friends = require("./app/data/friends");
 
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = process.env.port || 3000;
+var PORT = process.env.port;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
